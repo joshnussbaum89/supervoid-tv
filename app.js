34d351e67129eacbc0afd2bda3bd06cbbeb2338f
Josh Nussbaum <Joshnussbaum89@gmail.com>
2021-04-22 17:50:26 -0500

    add loading screen overlay

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
+const app = document.querySelector(".App");
 const hamburger = document.querySelector(".hamburger");
+const overlay = document.querySelector(".overlay");
+const overlayTitle = document.querySelector(".overlay h1");
+
+// Fade in overlay title
+setTimeout(() => {
+  overlayTitle.style.opacity = "1";
+}, 750);
+
+// Fade in bottom border
+setTimeout(() => {
+  overlayTitle.style.borderBottom = "2px solid #3700b3";
+}, 1500);
+
+// Make initial loading screen disapear
+setTimeout(() => {
+  overlay.style.opacity = "0";
+}, 3000);
+
+// Remove overlay entirely
+// Introduce the rest of the App
+setTimeout(() => {
+  overlay.style.display = "none";
+  app.style.display = "block";
+}, 4000);
+
+// Hamburger nav listener
 hamburger.addEventListener("click", (e) => {
   hamburger.classList.toggle("is-active");
 });
