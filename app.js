bf7ba187e38345c0ed3762ca7fa5dd100210e463
Josh Nussbaum <Joshnussbaum89@gmail.com>
2021-04-23 11:44:35 -0500

    refactor sass and JS

--- app.js
+++ app.js
@@ -1,31 +1,53 @@
-const app = document.querySelector(".App");
+/* ============================================= */
+/*              Variables                        */
+/* ============================================= */
+
 const hamburger = document.querySelector(".hamburger");
-const overlay = document.querySelector(".overlay");
-const overlayTitle = document.querySelector(".overlay h1");
 
-// Fade in overlay title
-setTimeout(() => {
-  overlayTitle.style.opacity = "1";
-}, 750);
+/**
+ * IIFE for initial loading screen
+ */
+(() => {
+  const app = document.querySelector(".App");
+  const overlay = document.querySelector(".overlay");
+  const overlayTitle = document.querySelector(".overlay h1");
 
-// Fade in bottom border
-setTimeout(() => {
-  overlayTitle.style.borderBottom = "2px solid #3700b3";
-}, 1500);
+  // Fade in overlay title
+  setTimeout(() => {
+    overlayTitle.style.opacity = "1";
+  }, 750);
 
-// Make initial loading screen disapear
-setTimeout(() => {
-  overlay.style.opacity = "0";
-}, 3000);
+  // Fade in bottom border
+  setTimeout(() => {
+    overlayTitle.style.borderBottom = "2px solid #050505";
+  }, 1500);
 
-// Remove overlay entirely
-// Introduce the rest of the App
-setTimeout(() => {
-  overlay.style.display = "none";
-  app.style.display = "block";
-}, 4000);
+  // Make initial loading screen disapear
+  setTimeout(() => {
+    overlay.style.opacity = "0";
+  }, 3000);
 
-// Hamburger nav listener
-hamburger.addEventListener("click", (e) => {
+  // Remove overlay entirely
+  // Introduce the rest of the App
+  setTimeout(() => {
+    overlay.style.display = "none";
+    app.style.display = "block";
+  }, 4000);
+})();
+
+/* ============================================= */
+/*              Functions                        */
+/* ============================================= */
+
+/**
+ * Hamburger nav listener
+ */
+const toggleHamburger = () => {
   hamburger.classList.toggle("is-active");
-});
+};
+
+/* ============================================= */
+/*              Event Listeners                  */
+/* ============================================= */
+
+hamburger.addEventListener("click", toggleHamburger);
